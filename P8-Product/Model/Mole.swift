adc8e3b6cebcca18e0b0a44badf9fcd69497043f
Skjodt <nskjod21@student.aau.dk>
2026-03-25 10:59:56 +0100

    Updated Mole.swift

--- P8-Product/Model/Mole.swift
+++ P8-Product/Model/Mole.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftData
+import SwiftUI
+import UIKit
+
+enum Frequency: String {
+    case monthly, quarterly, yearly
+}
+
+@Model
+final class Mole {
+    @Attribute(.unique) var id: UUID
+    var person: Person?
+    var name: String
+    var bodyPart: String
+    var isReminderActive: Bool = false
+    var reminderFrequency: Frequency?
+    var nextDueDate: Date?
+    
+    init(
+        id: UUID = UUID(),
+        person: Person? = nil,
+        name: String,
+        bodyPart: String,
+        isReminderActive: Bool = false,
+        reminderFrequency: Frequency,
+        nextDueDate: Date = Date()
+    ) {
+        self.id = id
+        self.person = person
+        self.name = name
+        self.bodyPart = bodyPart
+        self.isReminderActive = isReminderActive
+        self.reminderFrequency = reminderFrequency
+        self.nextDueDate = nextDueDate
+    }
+}
