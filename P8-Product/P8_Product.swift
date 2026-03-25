3280467aff93bb4b68e20369153e1ff4368535cf
Lazcse <lassefonager@hotmail.com>
2026-03-25 11:02:47 +0100

    Made the CI use in-memory store instead of the database

--- P8-Product/P8_Product.swift
+++ P8-Product/P8_Product.swift
@@ -9,6 +9,14 @@ import UIKit
 
 @main
 struct P8_Product: App {
+    var container: ModelContainer = {
+        if ProcessInfo.processInfo.environment["CI"] == "true" { // Use in-memory store for CI testing
+            let config = ModelConfiguration(isStoredInMemoryOnly: true)
+            return try! ModelContainer(for: MyModel.self, configurations: config)
+        } else {
+            return try! ModelContainer(for: MyModel.self)
+        }
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
